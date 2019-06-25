4eb0a158d0f598a1996856fa4376b741a94ce2a7
minhtus <mitu9811@gmail.com>
2019-06-25 14:28:47 +0700

    Change DbConnection for release

--- YourShares.Data/YourSharesContext.cs
+++ YourShares.Data/YourSharesContext.cs
@@ -364,7 +364,7 @@ namespace YourShares.Data
                 .Build();
 
             // define the database to use
-            optionsBuilder.UseSqlServer(config.GetConnectionString("TuConnection"));
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
         }
     }
 }
