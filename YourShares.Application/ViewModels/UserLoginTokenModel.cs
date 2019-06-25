5136abde2bd9a61c9d52d1ace2d0950256dc425e
minhtus <mitu9811@gmail.com>
2019-06-25 15:39:34 +0700

    Adjust LoginModel; Move authorize annotation to top

---
+++ YourShares.Application/ViewModels/UserLoginTokenModel.cs
@@ -0,0 +1,8 @@
+namespace YourShares.Application.ViewModels
+{
+    public class UserLoginTokenModel
+    {
+        public string Jwt { get; set; }
+        public string UserId { get; set; }
+    }
+}
